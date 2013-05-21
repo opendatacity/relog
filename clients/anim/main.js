adca961f40b75aaf928ae7400bf76cdaa89ea003
Micho <github@michael-kreil.de>
2013-05-21 10:48:41 +0200

    changing map size

--- clients/anim/main.js
+++ clients/anim/main.js
@@ -2,7 +2,7 @@ var currentTime = 60*(24*1+10.4);
 var clients = [];
 var context;
 var width = 980;
-var height = 500;
+var height = 490;
 var gridSize = 3;
 var nearFieldRadius = 30;
 
@@ -37,28 +37,6 @@ function init() {
 		clients[index] = {point:undefined, x:0, y:0, r:0, x0:0, y0:0, r0:0, index:index, lastEvent:0};
 		random[index] = Math.random();
 	});
-	
-	/*
-	var menu = $('#menu');
-	for (var i = 2; i < 16; i++) {
-		(function () {
-			var time = i*6*60;
-			var d = Math.floor(i/4);
-			var h = (i % 4)*6;
-			var node = $('<span class="button">T'+d+' '+h+':00</span>');
-			node.click(function () {
-				currentTime = time;
-				jump = true;
-				start();
-			})
-			menu.append(node);
-		})();
-	}
-
-	var node = $('<span class="button">Stop</span>');
-	node.click(stop);
-	menu.append(node);
-	*/
 
 	var index = -1;
 	for (var time = -60; time <= (4*24+1)*60; time++) {
